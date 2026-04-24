7d54d9fdc63e7643470b2f5f5c268050dd096d7f
lienhejb <lienhejb@gmail.com>
2026-04-24 10:27:44 +0700

    Create lunar-javascript.d.ts

---
+++ lunar-javascript.d.ts
@@ -0,0 +1,25 @@
+declare module 'lunar-javascript' {
+  export const Solar: {
+    fromDate(date: Date): {
+      getLunar(): {
+        getYear(): number;
+        getMonth(): number;
+        getDay(): number;
+        getYearGanZhi(): string;
+      };
+      getYear(): number;
+      getMonth(): number;
+      getDay(): number;
+    };
+  };
+
+  export const Lunar: {
+    fromYmd(year: number, month: number, day: number): {
+      getSolar(): {
+        getYear(): number;
+        getMonth(): number;
+        getDay(): number;
+      };
+    };
+  };
+}
